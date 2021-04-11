8d68ab39ddb037aa01efc5df36b628f20d4d33a5
Alexander Bondarenko <aenor.realm@gmail.com>
2021-04-11 16:30:22 +0300

    Sync to hackage

--- src/Foreign/Storable/Generic.hs
+++ src/Foreign/Storable/Generic.hs
@@ -17,34 +17,9 @@ Portability : portable
 {-#LANGUAGE UndecidableInstances #-}
 
 
-module Foreign.Storable.Generic (GStorable (..), Storable(..), getFilling) where
+module Foreign.Storable.Generic (GStorable (..), Storable(..)) where
 
 
 
 import Foreign.Storable (Storable(..))
-import GHC.Generics (Generic(..))
-
-import Foreign.Storable.Generic.Internal (GStorable (..), GStorable'(..))
-import Foreign.Storable.Generic.Instances
-import qualified Foreign.Storable.Generic.Tools as Tools
-
-
-------Association to Storable class-------
-
-instance {-# OVERLAPS #-} (GStorable a) => (Storable a) where
-    {-# INLINE sizeOf #-}
-    sizeOf      = gsizeOf
-    {-# INLINE alignment #-}
-    alignment   = galignment
-    {-# INLINE peekByteOff #-}
-    peekByteOff = gpeekByteOff
-    {-# INLINE pokeByteOff #-}
-    pokeByteOff = gpokeByteOff
-
-
--- | A helper to visualize layout.
-getFilling :: (Generic a, GStorable a, GStorable' (Rep a)) => a -> [Tools.Filling]
-getFilling x = Tools.getFilling $ zip sizes aligns
-    where sizes  = glistSizeOf'    gx
-          aligns = glistAlignment' gx
-          gx = from x
+import Foreign.Storable.Generic.Internal (GStorable (..))
